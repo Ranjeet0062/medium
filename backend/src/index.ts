e865c1ef5a770583937e950a00cb820b0189649e
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-06-15 09:15:13 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+import { userRouter } from "./routes/UserRoute";
+import { blogRouter } from "./routes/BlogRoute";
+import { cors } from "hono/cors";
+
+// Create the main Hono app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+app.use("/*", cors());
+
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
+app.get("/", (c) => {
+  return c.text("Hello Hono");
+});
+
+export default app;
